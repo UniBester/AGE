8e312828575250b602289dc5f641a7883e8af26e
Bester <384245293@qq.com>
2022-03-29 21:59:56 -0400

    1.5

--- options/test_options.py
+++ options/test_options.py
@@ -11,11 +11,11 @@ class TestOptions:
 		# arguments for inference script
 		self.parser.add_argument('--dataset_type', default='fl_encode', type=str, help='Type of dataset/experiment to run')
 		self.parser.add_argument('--psp_checkpoint_path', default='/data/dgq/fpsp/pretrained_models/psp_iteration_90000.pt', type=str, help='Path to pSp model checkpoint')
-		self.parser.add_argument('--checkpoint_path', default='/data/dgq/fpsp/flower/log_w0-2,3-5_0.0005or_wosigmoid_0.005l1_0dw_unlinear_multi_dense1001/checkpoints/iteration_35000.pt', type=str, help='Path to AGE model checkpoint')
-		self.parser.add_argument('--n_distribution_path', type=str, default='data1/', help='Path to distribution of n')
+		self.parser.add_argument('--checkpoint_path', default='experiment/logs/flowers/checkpoints/iteration_80000.pt', type=str, help='Path to AGE model checkpoint')
+		self.parser.add_argument('--n_distribution_path', type=str, default='data2/', help='Path to distribution of n')
 		self.parser.add_argument('--train_data_path', type=str, default='/data/dgq/fpsp/data/flower/train_without_cate', help='Path to directory of training set')
 		self.parser.add_argument('--test_data_path', type=str, default='/data/dgq/fpsp/flower/test_without_cate', help='Path to to directory of inference inputs')
-		self.parser.add_argument('--output_path', type=str, default='data1/output', help='Path to save outputs')
+		self.parser.add_argument('--output_path', type=str, default='data2/output', help='Path to save outputs')
 		self.parser.add_argument('--class_embedding_path', type=str, default='/data/dgq/fpsp/data/flower/flower_av_w.pt', help='Path to save class embeddings')
 		
 		self.parser.add_argument('--test_batch_size', default=2, type=int, help='Batch size for testing and inference')
@@ -24,7 +24,7 @@ class TestOptions:
 		self.parser.add_argument('--n_images', type=int, default=128, help='Number of images to generate on per input')
 		self.parser.add_argument('--A_length', default=100, type=int, help='Length of A')
 		self.parser.add_argument('--alpha', default=1, type=float, help='Editing intensity alpha')
-		self.parser.add_argument('--beta', default=0.005, type=float, help='Direction selection threshold in A')
+		self.parser.add_argument('--beta', default=0.000, type=float, help='Direction selection threshold in A')
 		self.parser.add_argument('--resize_outputs', action='store_true', help='Whether to resize outputs to 256x256 or keep at 1024x1024')
 	def parse(self):
 		opts = self.parser.parse_args()
